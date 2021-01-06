7418e4ef482588ffb95d2ca488d2dc7fe2e4dc09
Jeremiah Boby <mail@jeremiahboby.me>
2021-01-06 11:24:22 +0000

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+## 0.1.2
+- Combine author name fields into one
+- Add Category model
+- Update url to be more explicit
+- Add str test for new model
+
+## 0.1.2.1
+- add read time
+
+## 0.1.2.2
+- Change URL property on Article model to a method to remove error when using "view on site" 
+
+## 0.1.3
+- Add related article plugin 
+
+## 0.2.0
+- Add settings and files for tests to be run outside of a django project
---
+++ settings.py
@@ -0,0 +1,55 @@
+SECRET_KEY = "giant-people"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "giant-people",
+    }
+}
+
+INSTALLED_APPS = [
+    "cms",
+    "treebeard",
+    "menus",
+    "sekizai",
+    "easy_thumbnails",
+    "filer",
+    "mptt",
+    "djangocms_admin_style",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sites",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "people",
+]
+ROOT_URLCONF = "people.tests.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": ["people/templates"],
+        "OPTIONS": {
+            "context_processors": [
+                "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.request",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+MIDDLEWARE = [
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+]
+
+SITE_ID = 1
+
+LANGUAGE_CODE = "en-gb"
+
+LANGUAGES = [
+    ("en-gb", "English"),
+]
