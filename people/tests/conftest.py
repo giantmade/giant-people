4cff9a15ee3f3181aafb785f0faeafda0918f4c5
Will-Hoey <will.hoey@hotmail.co.uk>
2021-01-15 14:14:16 +0000

    Linting config

--- people/tests/conftest.py
+++ people/tests/conftest.py
@@ -4,6 +4,4 @@ from people.models import Person
 
 @pytest.fixture
 def person_instance():
-    return Person(
-        name="test person", linkedin_url="https://www.linkedin.com/in/testperson"
-    )
+    return Person(name="test person", linkedin_url="https://www.linkedin.com/in/testperson")
